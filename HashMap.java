9f43d41529bf41fa9b693f44c0f1b932c9faf676
Julia Zuara <julia_any@hotmail.com>
2015-02-25 14:07:55 +0100

    FInalizados metodos put y get del hashmap

--- HashMap.java
+++ HashMap.java
@@ -18,39 +18,79 @@ public class HashMap
         // initialise instance variables
         diccionario = new ArrayList<Celdilla>();
     }
-    
+
     /**
      * Metodo para introducir un valor al hashmap
+     * @param clave La clave a introducir
+     * @param valor El valor asociado a la clave
+     * @return Si la clave no existia, retorna -1. Si ya
+     *          existia sobreescribe el valor y lo retorna
      */
-    public void put(String clave, int valor)
+    public int put(String clave, int valor)
     {
-        if(comprobarClave(clave))
+        // Creamos el entero a retornar y una celdilla
+        // que tomaremos con un metodo interno que comprueba claves
+        // y devuelve el objeto asociado a esa clave.
+        Celdilla temp = comprobarClave(clave);
+        int retornar = -1;
+        // Si la clave ya existia, sobreescribo el valor y guardo el valor
+        // anterior para devolverlo
+        if(temp != null)
         {
+            retornar = temp.getValor();
+            temp.set(clave, valor);
         }
+        // Si no existia añado una nueva celdilla al diccionario
         else
         {
             diccionario.add(new Celdilla(clave, valor));
         }
+        return retornar;
     }
-    
+
+    /**
+     * Metodo que devuelve el valor asociado a una clave
+     * @param clave, La clave para buscar
+     * @return El valor asociado a la clave, o -1 si no existe la clave
+     */
+    public int get(String clave)
+    {
+        // Creamos el entero a retornar y una celdilla
+        // que tomaremos con un metodo interno que comprueba claves
+        // y devuelve el objeto asociado a esa clave.
+        Celdilla temp = comprobarClave(clave);
+        int retornar = -1;
+        // Si la clave ya existia, devuelvo el valor asociado
+        if(temp != null)
+        {
+            retornar = temp.getValor();
+        }
+        return retornar;
+    }
+
     /**
      * Metodo para comprobar si una clave ya existe en el hashmap
+     * @param clave La clave a comprobar si ya existe
+     * @return La celdilla si la encuentra, sino devolvera null
      */
-    private int comprobarClave(String clave)
+    private Celdilla comprobarClave(String clave)
     {
+        // Creamos una boolean y un indice, asi como la celdilla a devolver
         boolean encontrada = false;
         int indice = 0;
-        int devolver = -1;
+        Celdilla devolver = null;
+        // Recorremos la coleccion, si existe guardamos la celdilla correspondiente
+        // a esa clave y cambiamos el valor de la boolean
         while (indice < diccionario.size() && !(encontrada))
         {
             Celdilla temp = diccionario.get(indice);
             if( clave == temp.getClave())
             {
-                devolver = temp.get();
-                temp.set(clave, valor);
+                devolver = temp;
                 encontrada = true;
             }
         }
-        
+        return devolver;
     }
+
 }
